715a2f741cf8e561c6c9afa98ff851e7ff78e35d
SpoorthyS1 <spoorthy.s80@gmail.com>
2024-12-18 07:03:29 +0530

    connecting to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:developer';
-
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:social_nest/component/Service_tile.dart';
 import 'package:social_nest/pages/HomePage.dart';
@@ -9,8 +11,23 @@ import 'package:social_nest/pages/RegisterPage.dart';
 import 'package:social_nest/pages/Services.dart';
 import 'package:social_nest/pages/Settings.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: "AIzaSyBAnkSM-H_yDuqro_Nv_8D2Jw21MNURYNM",
+            authDomain: "socialnest-a6723.firebaseapp.com",
+            projectId: "socialnest-a6723",
+            storageBucket: "socialnest-a6723.firebasestorage.app",
+            messagingSenderId: "187036775104",
+            appId: "1:187036775104:web:92d274b8913adb2b477325"));
+    runApp(const MyApp());
+  } else {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
